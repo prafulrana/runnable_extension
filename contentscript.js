93dfbe832c2cb4e55c256c75e0db176339f5dbf0
Praful Rana <prafulrana@gmail.com>
2013-04-18 17:11:03 -0700

    Final fixes

--- contentscript.js
+++ contentscript.js
@@ -4,15 +4,46 @@
  * LICENSE file.
  */
 var stackOverFlowUrl = "stackoverflow.com";
+var runnable_url = "http://runnable.com";
 var runBadge = chrome.extension.getURL("favicon.ico");
+var nodeVersion = "node 0.6.x";
 
 function findRequiredPackages (codeText) {
-
+	var ctr = 0;
+	var results = [];
+	codeText.split("require(").forEach(function (require){
+		ctr++;
+		if (ctr > 1) {
+			var library = require.split(")").shift().replace(/['"]/g,'');
+			if (library.indexOf(".js") == -1 &&
+				library.indexOf(".git") == -1 &&
+				library != "http") {
+				results.push(library);
+			}
+		}
+	});
+	return results
 }
-var runnable_url = "http://localhost:3000";
+
+function getPackageJson (codeText) {
+	var packagekjson = '{\n"name": "HelloWorld",\n"author": "Runnable",\n"version": "0.0.1",\n"description": "Runnable Sample Application",\n"dependencies": {';
+	var ctr = 0;
+	findRequiredPackages(codeText).forEach(function (library){
+		if (ctr > 0)
+			packagekjson += ',\n"';
+		else
+			packagekjson += '\n"';
+
+		packagekjson += library + '":"*"';
+		ctr++;
+	});
+	packagekjson += '\n},\n"engine": "' + nodeVersion + '"\n}';
+    return packagekjson;
+}
+
 
 var handleClick = function (code) {
-	$.blockUI({ message: '<h1><img src="' + runBadge + '" /> Your Runnable is loading...</h1>' })
+	$.blockUI({ message: '<h1><img src="' + runBadge + '" /> Your Runnable is loading...</h1>' });
 	$.ajax({
 		url: runnable_url + "/api/projects/",
 		type:'POST',
@@ -23,18 +54,24 @@ var handleClick = function (code) {
 			$.ajax({
 			   url: runnable_url + "/api/projects/" + data._id + "/files/server.js",
 			   type: 'PUT',
-			   success: function(response) {
+			   success: function(response_server) {
 			   	// alert("checkout http://runnable.com/" + data._id);
-			   	window.location.replace(runnable_url + "/" + data._id);
+					$.ajax({
+					   url: runnable_url + "/api/projects/" + data._id + "/files/package.json",
+					   type: 'PUT',
+					   success: function(response_package) {
+					   	// alert("checkout http://runnable.com/" + data._id);
+
+
+					   	window.location.replace(runnable_url + "/" + data._id);
+					   },
+					   data: {"content": getPackageJson(code), "name": "package.json"}
+					});
 			   },
 			   data: {"content": code, "name": "server.js"}
 			});
 		}
 	});
-
-
-
-
 }
 
 // Test the text of the body element against our regular expression.
